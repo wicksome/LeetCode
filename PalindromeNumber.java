1a7e02c49e4bfaa51c583bac408c61221b564f74
yeongjun.kim <opid911@gmail.com>
2018-10-22 10:39:54 +0900

    Update PalindromeNumber.java

--- PalindromeNumber.java
+++ PalindromeNumber.java
@@ -4,35 +4,14 @@ class Solution {
         if (x / 10 == 0) return true;
         if (x > 10 && (x % 10) == 0) return false;
         
-        int idx = 0;
-        int temp = x;
+        final int origin = x;
+        int reversed = 0;
         
-        while (temp > 0) {
-            temp /= 10;
-            idx++;
+        while (x != 0) {
+            reversed = reversed * 10 + x % 10;
+            x /= 10;
         }
         
-        int right = idx - 1;
-        int leftValue = x;
-        int rightValue = x;
-        
-        for (int left = 0; left < idx / 2; left++) {
-            int l = leftValue % 10;
-            leftValue /= 10;
-
-            int offset = powOfTen(right--);
-            int r = rightValue / offset;
-            rightValue %= offset;
-            
-            if (r != l) {
-                return false;
-            }
-        }
-        
-        return true;
-    }
-    
-    private int powOfTen(int x) {
-        return (x > 1) ? 10 * powOfTen(x - 1) : 10;
+        return origin == reversed;
     }
 }
